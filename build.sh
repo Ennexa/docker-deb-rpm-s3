b769cdc59f9b8f308d8f865756f12dd6d355777c
Joyce Babu <joyce@ennexa.com>
2017-08-02 20:15:48 +0530

    Initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+#
+# Build script for the deb-rpm-s3 docker container.
+#
+
+cd "$(dirname "$0")"
+
+docker build -t ennexa/deb-rpm-s3 .
+
---
+++ deb-rpm-s3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#
+# Wrapper script for starting the docker container.
+#
+# You must set AWS_ACCESS_KEY and AWS_SECRET_KEY in your environment prior to
+# running. You can optionally pass the GPG key's passphrase as the environment
+# variable PASS.
+#
+
+cd "$(dirname "$0")"
+
+if [ ! -e "elasticsearch.asc" ]; then
+    cat << EOF
+You must place a copy of the Elasticsearch GPG signing key (named
+elasticsearch.asc) into
+
+  $PWD
+
+prior to building this docker image.
+
+EOF
+    exit 1
+fi
+
+bucket="packages.elasticsearch.org"
+prefix="beats"
+dir="/beats-packer/build/upload"
+gpg_key="/beats-packer/dev-tools/packer/docker/deb-rpm-s3/elasticsearch.asc"
+origin=Elastic
+
+docker run -it --rm \
+  --env="PASS=$PASS" \
+  --volume %cd%:/data \
+  deb-rpm-s3 \
+  --bucket=$bucket \
+  --prefix=$prefix \
+  --directory="$dir" \
+  --aws-access-key="$AWS_ACCESS_KEY" \
+  --aws-secret-key="$AWS_SECRET_KEY" \
+  --gpg-key="/data/conf/gpg.asc" \
+  --origin="Elastic" \
+  --verbose \
+  "$@"
